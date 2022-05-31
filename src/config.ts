1916fd2767d0d3865bfd561410d028598fdeafd1
Roy Arata <royarata0406@gmail.com>
2022-05-31 17:03:48 -0400

    add opensea js

--- src/config.ts
+++ src/config.ts
@@ -1,13 +1,2 @@
-import ERC20_ABI from 'builds/ERC20.json'
-import Farm_ABI from 'builds/Farm_ABI.json'
-
 export const ROUTERS = {
-    Farm: {
-        abi: Farm_ABI,
-        address: '0xfda1cf6261dcabaa29b3e464f78717ffb54b8a63',
-    },
-    PAX: {
-        abi: ERC20_ABI,
-        address: '0x3fD5D987eee6b6d5E590835DA55a12cf5658307d',
-    },
 }
--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,10 +1,10 @@
-import React, { useMemo, useState } from 'react'
+import React, { useMemo } from 'react'
 import styled from 'styled-components'
-import { Box, Button } from '@material-ui/core'
-import { parseEther } from '@ethersproject/units'
+import { Box, Button, Card } from '@material-ui/core'
 import { useWeb3React } from '@web3-react/core'
-import { useContract, usePAXContract } from 'hooks/useContracts'
-import { ROUTERS } from 'config'
+import { OpenSeaPort, Network } from 'opensea-js'
+import { OrderSide, OpenSeaAsset } from 'opensea-js/lib/types'
+import Web3 from 'web3'
 
 interface Props {
     setOpen: any;
@@ -12,82 +12,47 @@ interface Props {
 
 const Landing: React.FC<Props> = ({ setOpen }) => {
     const { account } = useWeb3React()
-    const farmContract = useContract(ROUTERS.Farm.address, ROUTERS.Farm.abi)
-    const tokenContract = usePAXContract()
 
-    const [deposited, setDeposited] = useState(0)
-    const [apr, setAPR] = useState(0)
-    const [count, setCount] = useState(0)
-    const [doing, setDoing] = useState(false)
-    const [amount, setAmount] = useState('0')
-    useMemo(async () => {
-        if (account && count >= 0) {
-            const _decimal = await tokenContract?.decimals().call()
-            const _deposited = parseFloat(await farmContract?.depositedTokens(account).call()) / (10 ** parseInt(_decimal))
-            const _total = parseFloat(await farmContract?.totalTokens().call()) / (10 ** parseInt(_decimal))
-            setDeposited(_deposited)
-            setAPR((_deposited / _total) * 100)
-        }
-    }, [account, count, tokenContract, farmContract])
+    const provider = new Web3.providers.HttpProvider('https://rinkeby.infura.io/v3/')
+    const seaport = new OpenSeaPort(provider, {
+        networkName: Network.Rinkeby,
+        apiKey: process.env.REACT_APP_API_KEY
+    })
 
-    const onApprove = async () => {
-        setDoing(true)
-        try {
-            await tokenContract?.approve(ROUTERS.Farm.address, '10000000000000000000000000000').send({ from: account }).on('transactionHash', (hash: any) => {
-                alert('confirm')
-            })
-            setCount(prev => prev + 1)
-        } catch (e) { console.log(e) }
-        setDoing(false)
+    const handleBuy = async () => {
+        const order = await seaport.api.getOrder({ side: OrderSide.Sell })
+        const accountAddress = "0x..." // The buyer's wallet address, also the taker
+        const transactionHash = await seaport.fulfillOrder({ order, accountAddress })
     }
 
-    const onStakeUnStake = async (stake: boolean) => {
-        setDoing(true)
-        try {
-            if (stake) await farmContract?.deposit(parseEther(amount).toString()).send({ from: account })
-            else await farmContract?.withdraw(parseEther(amount).toString()).send({ from: account })
-            setCount(prev => prev + 1)
-        } catch (e) {
-            console.log(e)
-        }
-        setDoing(false)
-    }
+    useMemo(async() => {
+        const asset: OpenSeaAsset = await seaport.api.getAsset({
+            tokenAddress: '0x88b48f654c30e99bc2e4a1559b4dcf1ad93fa656', // string
+            tokenId: '340963276596901265695437748819921892772811357280503139054330958001333075969', // string | number | null
+          })          
+    }, [])
 
-    const onClaim = async () => {
-        setDoing(true)
-        try {
-            await farmContract?.claim().send({ from: account })
-            setCount(prev => prev + 1)
-        } catch (e) {
-            console.log(e)
-        }
-        setDoing(false)
-    }
     return (
         <StyledContainer>
             <Box display={'flex'} justifyContent='space-between'>
-                Landing
+                NFT card shows from Opensea
                 {account ? <Button color='primary' variant='contained'>{account}</Button>
                     :
                     <Button color='primary' variant='contained' onClick={() => setOpen(true)}>connect wallet</Button>
                 }
             </Box>
-            <Button color='primary' variant='contained' disabled={!account || doing} onClick={onApprove}>Enable</Button>
-            <Box mt={3} display='flex' gridColumnGap={10}>
-                <input type='text' value={amount} onChange={(e) => setAmount(e.target.value)} />
-                <Button color='primary' variant='contained' disabled={!account || doing} onClick={() => onStakeUnStake(true)}>Stake</Button>
-                <Button color='secondary' variant='contained' disabled={!account || doing} onClick={() => onStakeUnStake(false)}>Unstake</Button>
-            </Box>
-            <Box>Staked: {deposited.toFixed(2)}</Box>
-            <Box>APR: {apr.toFixed(2)}%</Box>
-            <Box mt={3} display='flex' gridColumnGap={10}>
-                <Button color='primary' variant='contained' disabled={!account || doing} onClick={onClaim}>claim</Button>
-            </Box>
+            <NFTCard>
+                <Button onClick={handleBuy}>Buy</Button>
+            </NFTCard>
         </StyledContainer>
     )
 }
 
+const NFTCard = styled(Card)`
+`
+
 const StyledContainer = styled(Box)`
+    padding: 32px;
 `
 
 export default Landing
