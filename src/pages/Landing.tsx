86df8b4d637bd4ddc9dd19c0864e73b893296aac
Roy Arata <royarata0406@gmail.com>
2022-05-31 21:28:44 -0400

    add private key

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -4,7 +4,9 @@ import { Box, Button, Card } from '@material-ui/core'
 import { useWeb3React } from '@web3-react/core'
 import { OpenSeaPort, Network } from 'opensea-js'
 import { OrderSide, OpenSeaAsset } from 'opensea-js/lib/types'
-import Web3 from 'web3'
+import { MnemonicWalletSubprovider } from '@0x/subproviders'
+import RPCSubprovider from 'web3-provider-engine/subproviders/rpc'
+import Web3ProviderEngine from 'web3-provider-engine'
 
 interface Props {
     setOpen: any;
@@ -13,28 +15,43 @@ interface Props {
 const Landing: React.FC<Props> = ({ setOpen }) => {
     const { account } = useWeb3React()
     const [asset, setAsset] = useState<OpenSeaAsset>()
+    const [seaport, setSeaport] = useState<OpenSeaPort>()
+    const [doing, setDoing] = useState(false)
 
     const handleBuy = async () => {
-        const provider1 = new Web3.providers.HttpProvider('https://rinkeby.infura.io/v3/bb0e99ebaece4ea4bf1d2820cf1d0e23')
-        const seaport1 = new OpenSeaPort(provider1, {
-            networkName: Network.Rinkeby,
-        })
-        const order = await seaport1.api.getOrder({ side: OrderSide.Sell })
-        if (account) {
-            const transactionHash = await seaport1.fulfillOrder({ order, accountAddress: account })
-            console.log(transactionHash)
+        if (seaport && account) {
+            setDoing(true)
+            try {
+                const order = await seaport.api.getOrder({ side: OrderSide.Sell })
+                const transactionHash = await seaport.fulfillOrder({ order, accountAddress: account })
+                console.log(transactionHash)
+            } catch (e) {
+                console.log(e)
+            }
+            setDoing(false)
         }
     }
 
     useMemo(async () => {
-        const provider1 = new Web3.providers.HttpProvider('https://rinkeby.infura.io/v3/bb0e99ebaece4ea4bf1d2820cf1d0e23')
-        const seaport1 = new OpenSeaPort(provider1, {
+        const MNEMONIC = process.env.REACT_APP_MNEMONIC ?? ''
+        const INFURA_KEY = process.env.REACT_APP_INFURA_KEY ?? ''
+        const BASE_DERIVATION_PATH = `44'/60'/0'/0`
+        const mnemonicWalletSubprovider = new MnemonicWalletSubprovider({ mnemonic: MNEMONIC, baseDerivationPath: BASE_DERIVATION_PATH })
+        const infuraRpcSubprovider = new RPCSubprovider({
+            rpcUrl: 'https://rinkeby.infura.io/v3/' + INFURA_KEY,
+        })
+        const providerEngine = new Web3ProviderEngine()
+        providerEngine.addProvider(mnemonicWalletSubprovider)
+        providerEngine.addProvider(infuraRpcSubprovider)
+
+        const _seaport = new OpenSeaPort(providerEngine, {
             networkName: Network.Rinkeby,
         })
-        const _asset: OpenSeaAsset = await seaport1.api.getAsset({
+        const _asset: OpenSeaAsset = await _seaport.api.getAsset({
             tokenAddress: '0x88b48f654c30e99bc2e4a1559b4dcf1ad93fa656', // string
             tokenId: '340963276596901265695437748819921892772811357280503139054330958001333075969', // string | number | null
         })
+        setSeaport(_seaport)
         setAsset(_asset)
     }, [])
 
@@ -53,7 +70,7 @@ const Landing: React.FC<Props> = ({ setOpen }) => {
                         <Box>
                             <img src={asset?.imageUrl} alt='' />
                         </Box>
-                        <Button color='secondary' variant='contained' onClick={handleBuy}>Buy</Button>
+                        <Button color='secondary' variant='contained' onClick={handleBuy} disabled={!account || doing}>Buy</Button>
                     </NFTCard>
                     :
                     <Spinner width='40px' height='40px'>
